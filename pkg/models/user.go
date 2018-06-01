b8f3d09868a459de6f94e2670f770d9c4c8201f5
sueken5 <sueken0117@gmail.com>
2018-06-01 16:57:34 +0900

    iroriro

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -2,12 +2,14 @@ package models
 
 type User struct {
 	StudentID string `json:"student_id"`
+	NickName string `json:"nick_name"`
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
 	Gender int `json:"gender"` // 0 is male, 1 is female
 	Age int `json:"age"`
 	Grade int `json:"grade"`
-	Department string `json:"department"`
+	Faculty string `json:"faculty"` // 学部
+	Department string `json:"department"` //学科
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 }
